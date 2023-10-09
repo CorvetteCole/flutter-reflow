dcbf8fe5744b1ec9e76766f17944df703ddb83d4
Cole Gerdemann <cole@coleg.dev>
2023-10-09 14:57:20 -0400

    add libserialport

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # flutter-reflow
 A user interface for reflow ovens written in Flutter, meant for embedded Linux
+
+## Dependencies
+- `libserialport`
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:libserialport/libserialport.dart';
 
 void main() {
+
+  print('Available ports:');
+  var i = 0;
+  for (final name in SerialPort.availablePorts) {
+    final sp = SerialPort(name);
+    print('${++i}) $name');
+    print('\tDescription: ${sp.description}');
+    print('\tManufacturer: ${sp.manufacturer}');
+    print('\tSerial Number: ${sp.serialNumber}');
+    print('\tProduct ID: 0x${sp.productId!.toRadixString(16)}');
+    print('\tVendor ID: 0x${sp.vendorId!.toRadixString(16)}');
+    sp.dispose();
+  }
+
   runApp(const MyApp());
 }
 
