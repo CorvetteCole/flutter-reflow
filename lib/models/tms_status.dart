3e8e7dea36e86e0efea16c81fbc04ae1e14bf77b
Cole Gerdemann <cole@coleg.dev>
2023-10-31 09:02:50 -0500

    add up-to-date binary error codes for TMS, bring status model to "final" state

--- lib/models/tms_status.dart
+++ lib/models/tms_status.dart
@@ -1,46 +1,54 @@
-enum TmsState { idle, heating, cooling, error, unknown }
+enum TmsState { idle, heating, cooling, fault, unknown }
 
 enum TmsError {
-  none('No error'),
-  doorOpenedDuringHeating('Door opened during heating'),
-  targetTemperatureTooLow('Target temperature too low'),
-  targetTemperatureTooHigh('Target temperature too high'),
-  currentTemperatureTooLow('Current temperature too low'),
-  currentTemperatureTooHigh('Current temperature too high'),
-  currentTemperatureRoseTooQuickly('Current temperature rose too quickly'),
-  currentTemperatureFellTooQuickly('Current temperature fell too quickly'),
-  uiTimeout('UI timeout'),
-  unknown('Unknown error');
+  doorOpenedDuringHeating('Door opened during heating', 0x01),
+  targetTemperatureTooLow('Target temperature too low', 0x02),
+  targetTemperatureTooHigh('Target temperature too high', 0x04),
+  currentTemperatureTooLow('Current temperature too low', 0x08),
+  currentTemperatureTooHigh('Current temperature too high', 0x10),
+  currentTemperatureNotRisingDuringHeating(
+      'Current temperature not rising during heating', 0x20),
+  currentTemperatureFault('Fault while reading current temperature', 0x40),
+  uiTimeout('UI timeout', 0x80);
 
   final String friendlyMessage;
+  final int code;
 
-  const TmsError(this.friendlyMessage);
+  const TmsError(this.friendlyMessage, this.code);
 }
 
 class TmsStatus {
   final DateTime lastUpdated = DateTime.now();
   final int targetTemperature;
   final int currentTemperature;
+  final int topHeatDutyCycle;
+  final int bottomHeatDutyCycle;
   final TmsState state;
-  final TmsError error;
+  final int _error;
   final bool isDoorOpen;
 
-  TmsStatus({
-    required this.targetTemperature,
-    required this.currentTemperature,
-    required this.state,
-    required this.error,
-    required this.isDoorOpen,
-  });
+  List<TmsError> get errors {
+    return TmsError.values.where((e) => _error & e.code != 0).toList();
+  }
+
+  bool get hasErrors => _error != 0;
+
+  TmsStatus(
+      this.targetTemperature,
+      this.currentTemperature,
+      this.topHeatDutyCycle,
+      this.bottomHeatDutyCycle,
+      this.isDoorOpen,
+      this._error,
+      this.state);
 
   TmsStatus.fromJson(Map<String, dynamic> json)
       : targetTemperature = json['targetTemperature'],
         currentTemperature = json['currentTemperature'],
+        topHeatDutyCycle = json['topHeatDutyCycle'],
+        bottomHeatDutyCycle = json['bottomHeatDutyCycle'],
+        isDoorOpen = json['isDoorOpen'],
+        _error = json['error'],
         state = TmsState.values.firstWhere((e) => e.name == json['state'],
-            orElse: () => TmsState.unknown),
-        error = json['error']
-            ? TmsError.values.firstWhere((e) => e.name == json['error'],
-                orElse: () => TmsError.unknown)
-            : TmsError.none,
-        isDoorOpen = json['door_open'];
+            orElse: () => TmsState.unknown);
 }
