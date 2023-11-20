2a2add284fb8424e41f264b0e07efbb96d751422
Cole Gerdemann <cole@coleg.dev>
2023-11-19 21:03:02 -0500

    add to diag mode

--- lib/screens/diagnostic.dart
+++ lib/screens/diagnostic.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reflow/models/tms_status.dart';
+import 'package:provider/provider.dart';
 
 class DiagnosticsPage extends StatelessWidget {
   const DiagnosticsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Hello world'));
+    return Scaffold(
+      body: Consumer<TmsStatus>(builder: (context, TmsStatus status, child) {
+        return Column(children: [
+          Text('Current Temperature: ${status.currentTemperature}'),
+          Text('Target Temperature: ${status.targetTemperature}'),
+          Text('State ${status.state}'),
+          Text('Top Heater Duty Cycle ${status.topHeatDutyCycle}'),
+          Text('Bottom Heater Duty Cycle ${status.bottomHeatDutyCycle}'),
+          Text('Door Open ${status.isDoorOpen}'),
+          Text('Errors ${status.errors}'),
+        ]);
+      }),
+    );
   }
 }
